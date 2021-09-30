d24c75ccb3cc4ca9b40f174368ee8c150f69e35d
samoeun chhay <samoeunchh@gmail.com>
2021-09-30 09:42:14 +0700

    First commit

---
+++ SR2.API/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR2.API.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid BrandId { get; set; }
+        [MaxLength(50)]
+        [Required(ErrorMessage ="Brand name field is required")]
+        public string BrandName { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+    }
+}
